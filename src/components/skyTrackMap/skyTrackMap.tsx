0d99b6dbccac92a86942bc82e7c8d027fb4342db
Anton <fear.mega@bk.ru>
2025-08-07 23:49:16 +0300

    Add title and favicon

--- src/components/skyTrackMap/skyTrackMap.tsx
+++ src/components/skyTrackMap/skyTrackMap.tsx
@@ -1,13 +1,13 @@
 // src/components/sky-track-map/SkyTrackMap.tsx
 
-import AviationService from '@/services/aviantion.serves'
-import type { IFlight } from '@/services/aviation.types'
 import { useCurrentFlight } from '@/hooks/useCurrentFlight'
 import { useTheme } from '@/provider/useTheme'
+import AviationService from '@/services/aviantion.serves'
+import type { IFlight } from '@/services/aviation.types'
 import { useQuery } from '@tanstack/react-query'
 import { MapPin, Plane, PlaneTakeoff } from 'lucide-react'
 import 'maplibre-gl/dist/maplibre-gl.css'
-import { useEffect, useMemo, useRef, type RefObject } from 'react'
+import { useMemo, type RefObject } from 'react'
 import Map, { Layer, Marker, Source, type MapRef } from 'react-map-gl/maplibre'
 
 import {
@@ -15,7 +15,6 @@ import {
 	dashedStyle,
 	solidStyle
 } from './sky-track-map.utils'
-
 
 const getIntermediatePoint = (
 	from: [number, number],
@@ -28,16 +27,13 @@ const getIntermediatePoint = (
 	return [lat, lon]
 }
 
-interface ISkyTrackMap{
-	mapRef:RefObject<MapRef|null>
+interface ISkyTrackMap {
+	mapRef: RefObject<MapRef | null>
 }
 
-
-export function SkyTrackMap({mapRef}:ISkyTrackMap) {
+export function SkyTrackMap({ mapRef }: ISkyTrackMap) {
 	const { theme } = useTheme()
 
-	
-	
 	const { data: allFlights = [], isLoading: isLoadingAllFlights } = useQuery<
 		IFlight[],
 		Error
@@ -46,13 +42,11 @@ export function SkyTrackMap({mapRef}:ISkyTrackMap) {
 		queryFn: () => AviationService.getFlights()
 	})
 
-	
 	const { flight: selectedFlight } = useCurrentFlight()
 
-	
 	const otherFlightsCoordinates = useMemo(() => {
 		return allFlights
-			.filter(flight => flight.id !== selectedFlight?.id) 
+			.filter(flight => flight.id !== selectedFlight?.id)
 			.map(flight =>
 				getIntermediatePoint(
 					flight.from.coordinates,
@@ -61,7 +55,6 @@ export function SkyTrackMap({mapRef}:ISkyTrackMap) {
 				)
 			)
 	}, [allFlights, selectedFlight])
-
 
 	const currentLocation = useMemo(() => {
 		if (!selectedFlight) return null
@@ -93,17 +86,19 @@ export function SkyTrackMap({mapRef}:ISkyTrackMap) {
 		return createSplitGreatCircle(from, to, current)
 	}, [selectedFlight, currentLocation])
 
-
 	if (isLoadingAllFlights) {
-		return <div className='flex items-center justify-center h-screen bg-gray-200'>Loading Map...</div>
+		return (
+			<div className='flex h-screen items-center justify-center bg-gray-200'>
+				Loading Map...
+			</div>
+		)
 	}
 
 	return (
 		<Map
 			ref={mapRef}
-			
 			initialViewState={{
-				latitude: 48.85, 
+				latitude: 48.85,
 				longitude: 15.25,
 				zoom: 3.5
 			}}
@@ -114,35 +109,66 @@ export function SkyTrackMap({mapRef}:ISkyTrackMap) {
 					: 'https://basemaps.cartocdn.com/gl/positron-gl-style/style.json'
 			}
 		>
-		
-			
 			{selectedFlight ? (
 				<>
 					{solidFeature && solidFeature.geometry.coordinates.length > 1 && (
-						<Source id='route-solid' type='geojson' data={solidFeature}>
+						<Source
+							id='route-solid'
+							type='geojson'
+							data={solidFeature}
+						>
 							<Layer {...solidStyle(theme)}></Layer>
 						</Source>
 					)}
 					{dashedFeature && dashedFeature.geometry.coordinates.length > 1 && (
-						<Source id='route-dashed' type='geojson' data={{ type: 'FeatureCollection', features: [dashedFeature] }}>
+						<Source
+							id='route-dashed'
+							type='geojson'
+							data={{ type: 'FeatureCollection', features: [dashedFeature] }}
+						>
 							<Layer {...dashedStyle(theme)}></Layer>
 						</Source>
 					)}
 					{snappedPoint && (
-						<Marker latitude={snappedPoint[1]} longitude={snappedPoint[0]}>
-							<div style={{ transform: `rotate(${bearing - 45}deg)`, transformOrigin: 'center', transition: 'transform 0.3s ease' }}>
-								<Plane size={26} strokeWidth={0} className='fill-foreground' />
+						<Marker
+							latitude={snappedPoint[1]}
+							longitude={snappedPoint[0]}
+						>
+							<div
+								style={{
+									transform: `rotate(${bearing - 45}deg)`,
+									transformOrigin: 'center',
+									transition: 'transform 0.3s ease'
+								}}
+							>
+								<Plane
+									size={26}
+									strokeWidth={0}
+									className='fill-foreground'
+								/>
 							</div>
 						</Marker>
 					)}
 					{selectedFlight.from.coordinates?.length > 1 && (
-						<Marker latitude={selectedFlight.from.coordinates[0]} longitude={selectedFlight.from.coordinates[1]}>
-							<PlaneTakeoff size={30} className='' />
+						<Marker
+							latitude={selectedFlight.from.coordinates[0]}
+							longitude={selectedFlight.from.coordinates[1]}
+						>
+							<PlaneTakeoff
+								size={30}
+								className=''
+							/>
 						</Marker>
 					)}
 					{selectedFlight.to.coordinates?.length > 1 && (
-						<Marker latitude={selectedFlight.to.coordinates[0]} longitude={selectedFlight.to.coordinates[1]}>
-							<MapPin size={30} className='' />
+						<Marker
+							latitude={selectedFlight.to.coordinates[0]}
+							longitude={selectedFlight.to.coordinates[1]}
+						>
+							<MapPin
+								size={30}
+								className=''
+							/>
 						</Marker>
 					)}
 				</>
@@ -150,11 +176,15 @@ export function SkyTrackMap({mapRef}:ISkyTrackMap) {
 				<>
 					{otherFlightsCoordinates.map((coordinate, index) => (
 						<Marker
-							key={index} 
+							key={index}
 							latitude={coordinate[0]}
 							longitude={coordinate[1]}
 						>
-							<Plane size={24} className='fill-white/70 stroke-black/50' strokeWidth={0.5} />
+							<Plane
+								size={24}
+								className='fill-white/70 stroke-black/50'
+								strokeWidth={0.5}
+							/>
 						</Marker>
 					))}
 				</>
